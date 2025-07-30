b03020fe7e371b1147595bd31d82c459eac748c1
Shivang <shivangmathur101@gmail.com>
2025-07-31 01:38:07 +0530

    Understand Colocation and private Routes %5F

---
+++ app/_lib/page.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function PrivateRoute() {
+  return <h1>You cannot view this in the browser</h1>;
+}
+
+// to make route with underscore visible replace underscore with "%5F"
---
+++ app/dashboard/page.js
@@ -0,0 +1,17 @@
+// export default function BarChart() {
+//   return <h1>Bar Chart</h1>;
+// }
+
+// cant have 2
+
+function BarChart() {
+  return <h1>Bar Chart</h1>;
+}
+
+export default function DashBoard() {
+  return (
+    <>
+      <h1>DashBoard</h1>
+    </>
+  );
+}
